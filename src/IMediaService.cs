e35316a13e5721af8a0187230bae894376f2b653
Tarjei Olsen <tarjei.olsen@epinova.no>
2019-07-29 12:19:23 +0200

    Describe IMediaService contract

--- src/IMediaService.cs
+++ src/IMediaService.cs
@@ -4,7 +4,18 @@ namespace Epinova.IssuuMedia
 {
     public interface IMediaService
     {
+        /// <summary>
+        /// List all document embeds for the account
+        /// </summary>
+        /// <param name="apiKey">Application key for the account</param>
+        /// <param name="apiSecret">Secret for signing the request towards Issuu</param>
         Task<MediaDocumentEmbed[]> GetDocumentEmbedsAsync(string apiKey, string apiSecret);
+
+        /// <summary>
+        /// List all documents belonging to a user profile
+        /// </summary>
+        /// <param name="apiKey">Application key for the account</param>
+        /// <param name="apiSecret">Secret for signing the request towards Issuu</param>
         Task<MediaDocument[]> GetDocumentsAsync(string apiKey, string apiSecret);
     }
 }
