56faa8fe151ff267bdea6d81f48fa32045014d6f
Tarjei Olsen <tarjei.olsen@epinova.no>
2019-08-06 12:22:02 +0200

    Simplified API and added support for paging results

--- src/IMediaService.cs
+++ src/IMediaService.cs
@@ -5,17 +5,12 @@ namespace Epinova.IssuuMedia
     public interface IMediaService
     {
         /// <summary>
-        /// List all document embeds for the account
-        /// </summary>
-        /// <param name="apiKey">Application key for the account</param>
-        /// <param name="apiSecret">Secret for signing the request towards Issuu</param>
-        Task<MediaDocumentEmbed[]> GetDocumentEmbedsAsync(string apiKey, string apiSecret);
-
-        /// <summary>
         /// List all documents belonging to a user profile
         /// </summary>
         /// <param name="apiKey">Application key for the account</param>
         /// <param name="apiSecret">Secret for signing the request towards Issuu</param>
-        Task<MediaDocument[]> GetDocumentsAsync(string apiKey, string apiSecret);
+        /// <param name="pageSize">Maximum number of documents to be returned. Value must be between 0 - 30. Default is 10</param>
+        /// <param name="startIndex">Zero based index to start pagination from</param>
+        Task<MediaDocument[]> GetDocumentsAsync(string apiKey, string apiSecret, int pageSize = 10, int startIndex = 0);
     }
 }
